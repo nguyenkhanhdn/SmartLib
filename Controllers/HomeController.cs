52cd78074c54bf5c6b7fe4706007d664f669c368
Khanh Nguyen <khanhdsp@gmail.com>
2022-07-24 18:47:33 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using SmartLib.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace SmartLib.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private SmartLibEntities db = new SmartLibEntities();
+
+        // GET: Books
+        public ActionResult Index()
+        {
+            var books = db.Books.Include(b => b.Category);
+            var queries = books.OrderBy(r => Guid.NewGuid()).Take(3);
+            return View(queries.ToList());
+        }
+        //Guid: Global unique identifier
+        public ActionResult Books()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
